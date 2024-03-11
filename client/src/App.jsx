1cbb2828933e1f52002575d5f006abee20472c15
ajabadi <ajabadi@ucdavis.edu>
2024-03-11 11:40:13 -0700

    added the login and signup alogn with the homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,47 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import Navbar from './components/Navbar';
+import { AuthProvider } from './context/AuthContext';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Home from './components/Home';
 import './App.css';
-import { Outlet } from 'react-router-dom';
 
-// Uncomment import statement below after building queries and mutations
-// import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="flex-column justify-center align-center min-100-vh bg-primary">
-      <Outlet />
-    </div>
+    <ApolloProvider client={client}>
+      <AuthProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </AuthProvider>
+    </ApolloProvider>
   );
 }
+
 
 export default App;
